381018b5855f8442f043ebfd550f4bc7c6c0720e
leela18 <leesirvi@gmail.com>
2019-01-18 10:46:45 +0100

    FirstPythonCommit

---
+++ src/new_main.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Praveen"
+__date__ = "$18 Jan, 2019 10:42:37 AM$"
+
+if __name__ == "__main__":
+    print "Hello World";
